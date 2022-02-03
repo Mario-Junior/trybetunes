2abe7e42f64556d0c9f5cbce64109c40ef527b10
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-03 17:34:41 -0300

    2nd mandatory requirement update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,62 @@
 import React, { Component } from 'react';
 import './login.css';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
+
+const minNameSize = 3;
 
 class Login extends Component {
+  state = {
+    nameInput: '',
+    loading: false,
+    redirect: false,
+  }
+
+  componentWillUnmount() {
+    this.setState = () => {};
+  }
+
+  handleSubmitButton = async (event) => {
+    event.preventDefault();
+
+    this.setState({ loading: true });
+    const { nameInput } = this.state;
+
+    await createUser({ name: nameInput });
+
+    this.setState({ redirect: true });
+  };
+
   render() {
-    return (
-      <div data-testid="page-login">
+    const { nameInput, loading, redirect } = this.state;
+    const enableButton = nameInput.length >= minNameSize;
+    const form = (
+      <>
         <input
           type="text"
           data-testid="login-name-input"
-          name="name-input"
+          name="nameInput"
+          value={ nameInput }
           placeholder="Nome de usuário"
+          onChange={ (event) => this.setState({ nameInput: event.target.value }) }
         />
         <button
           type="button"
           data-testid="login-submit-button"
-          name="submit-button"
+          name="submitButton"
+          disabled={ !enableButton }
+          onClick={ this.handleSubmitButton }
         >
           Entrar
         </button>
+      </>
+    );
+
+    return (
+      <div data-testid="page-login">
+        { loading ? <Loading /> : form }
+        { redirect && <Redirect to="/search" /> }
       </div>
     );
   }
