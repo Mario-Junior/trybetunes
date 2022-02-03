9ab2583befa70b15a8b49799cfc5b593ca2837be
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-03 15:23:43 -0300

    1st mandatory requirement update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Login from './pages/Login';
 import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
 
 class App extends React.Component {
   render() {
@@ -9,6 +11,8 @@ class App extends React.Component {
         <p>TrybeTunes</p>
         <Login />
         <Search />
+        <Album />
+        <Favorites />
       </>
     );
   }
---
+++ src/pages/Favorites.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites">
+        Eu sou a página de Favorites
+      </div>
+    );
+  }
+}
+
+export default Favorites;
