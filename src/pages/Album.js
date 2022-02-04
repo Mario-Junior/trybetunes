e8a0bf5c6ba45b12e96c5e40d27f5348b75b2f47
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-04 13:00:25 -0300

    7th mandatory requirement - songs request

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  state = {}
+
+  handleMusics = async () => {
+    const { match: { params: { id } } } = this.props;
+
+    const musics = await getMusics(id);
+
+    console.log(musics);
+  }
+
   render() {
     return (
       <div data-testid="page-album">
         <Header />
+        <button type="button" onClick={ this.handleMusics }>Musicas</button>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    isExact: PropTypes.bool,
+    params: PropTypes.objectOf(PropTypes.string),
+    path: PropTypes.string,
+    url: PropTypes.string,
+  }),
+}.isRequired;
 
 export default Album;
