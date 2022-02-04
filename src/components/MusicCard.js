9cda6453487fa15225965dfd91488b1d1366047f
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-04 14:17:17 -0300

    7th mandatory requirement - music card component

---
+++ src/components/MusicCard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends Component {
+  render() {
+    const { trackname, previewUrl, image } = this.props;
+
+    return (
+      <div className="music-card">
+        { image && <img src={ image } alt={ `Album da música ${trackname}` } /> }
+        <h3>{ trackname }</h3>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackname: PropTypes.string,
+  previewUrl: PropTypes.string,
+  image: PropTypes.string,
+}.isRequired;
+
+export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -2,23 +2,63 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
-  state = {}
+  state = {
+    musicsArray: '',
+    loading: false,
+  }
+
+  componentDidMount() {
+    this.handleMusics();
+  }
 
   handleMusics = async () => {
     const { match: { params: { id } } } = this.props;
 
+    this.setState({ loading: true });
+
     const musics = await getMusics(id);
 
+    this.setState({ musicsArray: musics, loading: false });
     console.log(musics);
+  }
+
+  showMusics = () => {
+    const { musicsArray, loading } = this.state;
+    const { artistName, artworkUrl100, collectionName } = musicsArray[0];
+
+    return (
+      loading
+        ? <Loading />
+        : (
+          <section className="album_selected">
+            <div className="data-album">
+              <img src={ artworkUrl100 } alt="Capa do album" />
+              <h1 data-testid="album-name">{ collectionName }</h1>
+              <h2 data-testid="artist-name">{ artistName }</h2>
+            </div>
+
+            { musicsArray.slice(1).map((music) => {
+              const { artworkUrl30, previewUrl, trackId, trackName } = music;
+              return (
+                <MusicCard
+                  key={ trackId }
+                  image={ artworkUrl30 }
+                  trackname={ trackName }
+                  previewUrl={ previewUrl }
+                />);
+            })}
+          </section>
+        ));
   }
 
   render() {
     return (
       <div data-testid="page-album">
         <Header />
-        <button type="button" onClick={ this.handleMusics }>Musicas</button>
       </div>
     );
   }
