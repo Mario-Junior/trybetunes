1a9176a937db010a3729c0c2c8e40b94f3a3fbbb
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-02-06 16:03:42 -0300

    13th bonus requirement - displaying person data

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
@@ -15,9 +16,33 @@ class Profile extends Component {
   }
 
   render() {
+    const { loading, user: { name, email, image, description } } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
+        { loading ? <Loading />
+          : (
+            <section className="profile">
+              <div className="image">
+                { image.length
+                  ? <img data-testid="profile-image" src={ image } alt={ name } />
+                  : <img src="https://bit.ly/3B3AFwj" alt="imagem genérica para pessoa usuária" />}
+              </div>
+              <div className="section">
+                <h3>Nome</h3>
+                <p>{ name }</p>
+              </div>
+              <div className="section">
+                <h3>E-mail</h3>
+                <p>{ email }</p>
+              </div>
+              <div className="section">
+                <h3>Descrição</h3>
+                <p>{ description }</p>
+              </div>
+            </section>
+          )}
       </div>
     );
   }
