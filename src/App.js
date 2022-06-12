e3fb759bc4c619b466afb4dbc46745d976491f43
Mario-Junior <mariojuniorprofissional@gmail.com>
2022-06-11 23:50:34 -0300

    feat: setting up styled-components
    - global styles and default theme folders and files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,21 +9,27 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import GlobalStyles from './assets/styles/global';
+
+import { defaultTheme } from './assets/styles/themes/default';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route exact path="/profile/edit" component={ ProfileEdit } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route path="*" component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+        <GlobalStyles />
+      </ThemeProvider>
     );
   }
 }
---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  colors: {
+    background: '#EDF2F4',
+    primary: {
+      background: '#43919B',
+      main: '#30AADD',
+    },
+  },
+};
