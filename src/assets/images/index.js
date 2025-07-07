9306110f81e4f84c15308c47d9adce386df57436
Mário Júnior <mariojuniorprofissional@gmail.com>
2025-07-06 21:39:14 -0300

    refactor: Change of login and header logos

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,7 @@
 // /src/assets/images
 export { default as babyLogin } from './baby-login.png';
-export { default as headerLogo } from './header-logo.png';
+export { default as headerLogo } from './tunes-io-header.png';
 export { default as heartEmpty } from './heart-empty.svg';
 export { default as heartFull } from './heart-full.svg';
-export { default as loginLogo } from './login-logo.png';
+export { default as loginLogo } from './tunes-io-login.png';
 export { default as peopleMusic } from './people-music.webp';
